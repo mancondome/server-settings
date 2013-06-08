dcfea674bf33ebec3ec02573bbf8e6eb500240a6
mancondome <toyoshiko45@gmail.com>
2013-06-08 22:37:13 +0900

    UFW設定するように変更。

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 echo "`whoami` ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/`whoami` > /dev/null
 sudo chmod 440 /etc/sudoers.d/`whoami` 2> /dev/null
 
-sudo apt-get install git
+sudo ufw allow ssh
+sudo ufw enable
