83c02d4ef0a19fe1117186ebe945f5ada1a184a4
mancondome <toyoshiko45@gmail.com>
2013-06-16 23:37:26 +0900

    ip確認アプリ設定。

--- node.sh
+++ node.sh
@@ -1,5 +1,8 @@
+# install
 sudo apt-get install npm
 sudo useradd -m -d /var/lib/node -s /bin/false node
+
+# Nginx setting
 sudo mkdir /var/log/nginx/node 2> /dev/null
 cat <<EOF | sudo tee /etc/nginx/sites-available/node > /dev/null
 upstream node {
@@ -12,9 +15,36 @@ server {
   access_log /var/log/nginx/node/access.log;
   error_log /var/log/nginx/node/error.log;
 
+  proxy_redirect                          off;
+  proxy_set_header Host                   $host;
+  proxy_set_header X-Real-IP              $remote_addr;
+  proxy_set_header X-Forwarded-Host       $host;
+  proxy_set_header X-Forwarded-Server     $host;
+  proxy_set_header X-Forwarded-For        $proxy_add_x_forwarded_for;
+
   location / {
   	proxy_pass http://node;
   }
 }
 EOF
 sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node
+
+# log directory
+sudo mkdir /var/log/node
+sudo chown node:node /var/log/node/
+
+# install ipcheck application
+cat <<EOF | sudo -u node tee /var/lib/node/ipcheck.js
+var httpd = require('http').createServer(function(request, response) {
+    response.setHeader('Content-Type', 'text/plain');
+    response.write(request.headers['x-real-ip']);
+    response.end();
+});
+httpd.listen(3000);
+EOF
+
+cat <<EOF | sudo tee /etc/profile.d/node_init.sh > /dev/null
+(node /var/lib/node/ipcheck.js >> /var/log/node/ipcheck.log) 2>> /var/log/node/ipcheck_error.log &
+EOF
+sudo chown node:node /etc/profile.d/node_init.sh
+sudo chmod 4700 /etc/profile.d/node_init.sh
