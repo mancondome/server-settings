b7c872af0f6e059c742ca1dfaf8a775f4cb31093
mancondome <toyoshiko45@gmail.com>
2013-11-24 15:44:11 +0900

    設定変更。

--- chef.sh
+++ chef.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # chef-server
-sudo dpkg -i https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb
+DEB_PACKAGE='chef-server_11.0.8-1.ubuntu.12.04_amd64.deb'
+wget -q -nc -P /tmp/ https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$DEB_PACKAGE
+sudo dpkg -i /tmp/$DEB_PACKAGE
+sudo chef-server-ctl reconfigure
 # chef-client
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
